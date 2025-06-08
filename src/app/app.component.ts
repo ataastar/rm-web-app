94d95dcba3be61b8bb3d80abbf3faa0bede8dc8d
Molnar Attila <molnar.attila@otpmobil.com>
2025-06-08 21:35:00 +0200

    app component layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BranchSelectorComponent } from './branch-selector/branch-selector.component';
 import { RequirementTreeSelectorComponent } from './requirement-tree-selector/requirement-tree-selector.component';
-import { RequirementNodeComponent } from './requirement-node/requirement-node.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BranchSelectorComponent, RequirementTreeSelectorComponent, RequirementNodeComponent],
+  imports: [RouterOutlet, BranchSelectorComponent, RequirementTreeSelectorComponent, NgStyle],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'rm-web-app';
+
+  isResizing = false;
+  sidebarWidth = 250; // init width
+  readonly minWidth = 150;
+  readonly maxWidth = 500;
+
+  constructor() {
+  }
+
+  onMouseDown(event: MouseEvent) {
+    this.isResizing = true;
+    event.preventDefault(); // do not select text when clicking and moving
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (!this.isResizing) return;
+    this.sidebarWidth = Math.max(this.minWidth, Math.min(event.clientX, this.maxWidth));
+  }
+
+  @HostListener('document:mouseup')
+  onMouseUp() {
+    this.isResizing = false;
+  }
 }
